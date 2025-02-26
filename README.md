1630d94ad13c2f7675c075ad8570be6f8a8188dc
teddyvicker <vicpoliveira@gmail.com>
2025-02-26 13:46:51 -0400

    create implementado com sucesso

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 #TASK CRUD#
 
-- Criar a primeira task 
+- Criar a primeira task
 - Ler a primeira task
 - Atualizar a task
 - Deletar a task
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from models.tasks import Task
 
 
@@ -9,12 +9,18 @@ app = Flask(__name__)
 # Creat, read, Update and Delete
 # Tabela : Tarefa 
 tasks = []
+task_id_control = 1
 
 @app.route('/tasks', methods=['POST'])
 def create_task():
+    global task_id_control
     data = request.get_json()
     print(data)
-    return 'Test'
+    new_task = Task(id=task_id_control, title=data['title'], description=data.get("description",""))
+    task_id_control += 1
+    tasks.append(new_task)
+    print(tasks)
+    return jsonify({"message": "Nova tarefa criada com sucesso!"})
 
 if __name__ == "__main__":
     app.run(debug=True)
