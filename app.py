54c25ae38f43a050019c2fdf3e92e2d2ddf92cef
teddyvicker <vicpoliveira@gmail.com>
2025-02-26 12:55:55 -0400

    input da primeira task

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
-from flask import Flask
+from flask import Flask, request
+from models.tasks import Task
+
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "Hellow World!"
 
-@app.route("/about")
-def about():
-    return "Página Sobre"
+# CRUD
+# Creat, read, Update and Delete
+# Tabela : Tarefa 
+tasks = []
+
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    data = request.get_json()
+    print(data)
+    return 'Test'
 
 if __name__ == "__main__":
     app.run(debug=True)
