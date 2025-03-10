aea174f3d6f190c88c78ecccbd69ab6963cebe18
teddyvicker <vicpoliveira@gmail.com>
2025-03-10 12:37:45 -0400

    implementação do CRUD

--- app.py
+++ app.py
@@ -38,5 +38,37 @@ def get_task(id):
             return jsonify(t.to_dict())
     return jsonify({"message": "Não foi possível encontrar a ativdade"}), 404
 
+@app.route('/tasks/<int:id>', methods=["PUT"])
+def update_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t
+    print(task)        
+    if task == None:
+        return jsonify({"message": "Não foi possível encontrar a Atividade"}), 404
+    
+    data = request.get_json()
+    task.title = data['title']   
+    task.description = data['description']
+    task.completed = data['completed']
+    print(task)
+    return jsonify({"message": "Tarefa atualizada com sucesso"}), 200
+
+@app.route('/tasks/<int:id>', methods=["DELETE"])
+def delete_task(id):
+    task = None
+    for t in tasks:
+        print(t.to_dict())
+        if t.id == id:
+            task = t
+            break
+    
+    if not task:
+        return jsonify({"message": "Não foi possível encontrar a atividade"}),404
+    
+    tasks.remove(task)
+    return jsonify({"message": "Tarefa deletada com sucesso"})
+
 if __name__ == "__main__":
     app.run(debug=True)
